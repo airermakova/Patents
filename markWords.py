a4e317b3ffaa0aa83d59d46ec34956cc3a5187e5
airermakova <56444081+airermakova@users.noreply.github.com>
2022-02-20 20:55:47 +0100

    Add files via upload

--- markWords.py
+++ markWords.py
@@ -1,18 +1,28 @@
 import re
-
+tokenized_phrases = []
 
 def getWordsFromFile(fileName):
     phrases= []
-    tokenized_phrases = []
+    
     try:
        with open(fileName, 'r') as file:
            phrases = file.read().split(".")
        for phrase in phrases:
            tokenized_phrases.append(re.split(r'\W+', phrase))
-       print(tokenized_phrases)
        return tokenized_phrases
     except:
       print("Error in reading " + fileName)
       exit()
 
+def markPhrases():
+    f = open("taggedPhrases.txt", "a")
+    for tokenized_phrase in tokenized_phrases:
+        for word in tokenized_phrase:
+            if len(word)>0: 
+                word = word + "(N)"
+                f.write(word + " ")
+        f.write("\n")
+f = open("taggedPhrases.txt", "w")
+f.close()
 getWordsFromFile("Phrases.txt")
+markPhrases()
