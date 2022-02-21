df6ca1773071ecaf56ffc35864177a6f2be3c0eb
airermakova <56444081+airermakova@users.noreply.github.com>
2022-02-21 10:55:28 +0100

    Add files via upload

--- markWords.py
+++ markWords.py
@@ -1,14 +1,23 @@
 import re
+import nltk
+import numpy
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+from nltk import word_tokenize,pos_tag
+
+
 tokenized_phrases = []
 
 def getWordsFromFile(fileName):
-    phrases= []
-    
+    phrases= []    
     try:
        with open(fileName, 'r') as file:
            phrases = file.read().split(".")
        for phrase in phrases:
-           tokenized_phrases.append(re.split(r'\W+', phrase))
+           #tokenized_phrases.append(re.split(r'\W+', phrase))
+           tokenized_phrases.append(word_tokenize(phrase))
        return tokenized_phrases
     except:
       print("Error in reading " + fileName)
@@ -17,11 +26,10 @@ def getWordsFromFile(fileName):
 def markPhrases():
     f = open("taggedPhrases.txt", "a")
     for tokenized_phrase in tokenized_phrases:
-        for word in tokenized_phrase:
-            if len(word)>0: 
-                word = word + "(N)"
-                f.write(word + " ")
-        f.write("\n")
+        tag=pos_tag(tokenized_phrase)
+        ne_tree = nltk.ne_chunk(tag)
+        line = ' '.join(str(x) for x in ne_tree)
+        f.write(line + "\n")
 f = open("taggedPhrases.txt", "w")
 f.close()
 getWordsFromFile("Phrases.txt")
