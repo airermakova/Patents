266eadaf51ac8da4e151dff70c6b10779e83b31b
airermakova <56444081+airermakova@users.noreply.github.com>
2022-04-29 17:37:29 +0200

    Add files via upload

--- FlaiTest.py
+++ FlaiTest.py
@@ -1,5 +1,10 @@
 from flair.data import Corpus
 from flair.datasets import ColumnCorpus
+from flair.embeddings import WordEmbeddings, StackedEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+
+
 
 # define columns
 columns = {0: 'text', 1: 'pos', 2: 'ner'}
@@ -25,3 +30,35 @@ label_dict = corpus.make_label_dictionary(label_type=label_type)
 
 print("DICTIONARY READ")
 print(label_dict)
+
+# 4. initialize embeddings
+embedding_types = [
+
+    WordEmbeddings('glove'),
+
+    # comment in this line to use character embeddings
+    # CharacterEmbeddings(),
+
+    # comment in these lines to use flair embeddings
+    # FlairEmbeddings('news-forward'),
+    # FlairEmbeddings('news-backward'),
+]
+embeddings = StackedEmbeddings(embeddings=embedding_types)
+print("EMBEDDINGS")
+
+# 5. initialize sequence tagger
+tagger = SequenceTagger(hidden_size=256,
+                        embeddings=embeddings,
+                        tag_dictionary=label_dict,
+                        tag_type=label_type,
+                        use_crf=True)
+
+# 6. initialize trainer
+trainer = ModelTrainer(tagger, corpus)
+
+# 7. start training
+trainer.train("C:/Users/airer/Documents/Pisa/Classifier/trainer",
+              learning_rate=0.1,
+              mini_batch_size=32,
+              max_epochs=10)
+print("FINISH TRAINING")
